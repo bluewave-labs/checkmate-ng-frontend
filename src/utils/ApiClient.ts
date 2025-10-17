1e8a43cd433e4e42eabeeea1fc229a0916a1a42a
Alex Holliday <ajhollid@gmail.com>
2025-10-17 09:47:38 -0700

    401 interceptor

--- src/utils/ApiClient.ts
+++ src/utils/ApiClient.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import type { AxiosError } from "axios";
 import type { AxiosRequestConfig, AxiosResponse } from "axios";
 const BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
 
@@ -6,6 +7,16 @@ const api = axios.create({
   baseURL: BASE_URL || "http://localhost:55555/api/v2",
   withCredentials: true,
 });
+
+const onSuccess = (response: AxiosResponse) => response;
+const onError = (error: AxiosError) => {
+  if (error.response?.status === 401) {
+    window.location.href = "/login";
+  }
+  return Promise.reject(error);
+};
+
+api.interceptors.response.use(onSuccess, onError);
 
 export const get = <T>(
   url: string,
