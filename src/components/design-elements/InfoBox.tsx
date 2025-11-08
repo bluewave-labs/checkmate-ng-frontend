5c18bd4a400904ae1e841a8219ed15af543f727b
Alex Holliday <ajhollid@gmail.com>
2025-11-08 07:53:52 -0800

    baseline

---
+++ src/components/design-elements/InfoBox.tsx
@@ -0,0 +1,14 @@
+export const InfoBox = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+};
